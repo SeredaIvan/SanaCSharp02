d1b883fc9bc85cf4b605eae6be06315c9a5a9a66
SeredaIvan <vt221_bio@student.ztu.edu.ua>
2022-12-31 21:45:43 +0200

    first tsk compl

---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a= Convert.ToInt32(Console.ReadLine());
+int b= Convert.ToInt32(Console.ReadLine());
+int c= Convert.ToInt32(Console.ReadLine());
+double D = (b*b)-4*a*c;
+Console.WriteLine(D);
+if (D > 0)
+{
+    double x1 = (-b + Math.Pow(D, 1.0 / 2)) / (2.0 * a);
+    double x2 = (-b - Math.Pow(D, 1.0 / 2)) / (2.0 * a);
+    Console.WriteLine(x1);
+    Console.WriteLine(x2);
+}
+else if (D == 0)
+{
+    double x = (-b) / 2.0 * a;
+    Console.WriteLine(x);
+} 
+else
+{
+    Console.WriteLine("Коренів не існує");
+}
